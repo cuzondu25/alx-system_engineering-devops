954eec7e28cbbae07d85c805a894d5b284aa16d0
cuzondu25 <cuzondu25@gmail.com>
2024-05-30 14:47:31 +0100

    api advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""queries the Reddit API"""
+import requests
+
+def top_ten(subreddit):
+    """ function that queries the Reddit API and prints the titles 
+    of the first 10 hot posts listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    header = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    param = {"limit": 10}
+    response = requests.get(url, headers=header, params=param,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+    else:
+        results = response.json().get("data")
+        [print(c.get("data").get("title")) for c in results.get("children")]
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit."""
+
+import requests
+
+def recurse(subreddit,hot_list=[], after="", count=0):
+    """ a recursive function that queries the Reddit API
+    Return
+        A list containing the titles of all hot articles for a given subreddit
+        None is no result was found
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    header = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    params = {"after": after, "count": count, "limit": 100}
+    response = requests.get(url, headers=header, params=params, allow_redirects=False)
+
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for r in results.get("children"):
+        hot_list.append(r.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
