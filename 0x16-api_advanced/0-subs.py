1089f71a16429be48a1975536134e8fb23afaca7
cuzondu25 <cuzondu25@gmail.com>
2024-06-05 15:31:26 +0000

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,16 +4,13 @@ returns the number of subscribers"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return
-         number of subscribers (not active users, total subscribers)
-         If not a valid subreddit, return 0.
-    """
-
-
+    """Return number of subscribers (not active users, total subscribers)"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(url, headers=header, allow_redirects=False)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    result = response.json().get("data")
+    results = response.json().get("data")
     return result.get("subscribers")
